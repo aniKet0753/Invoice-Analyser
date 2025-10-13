51c33ef482fc319cf975418d6dabecb7da8580a7
/\n!KeT KuMaR <ankitrazz001@gmail.com>
2025-10-13 22:17:21 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const instance = axios.create({
   // baseURL: import.meta.env.VITE_API_BASE_URL,
-   baseURL: "http://localhost:5001/",
+   //baseURL: "http://localhost:5001/",
+  baseURL: 'https://pdf-intelligence.onrender.com',
   headers: {
     'Content-Type': 'application/json' 
   }
