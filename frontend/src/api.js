bcf35bc7ec9abdda606a76475e76cdf0db73345a
/\n!KeT KuMaR <ankitrazz001@gmail.com>
2025-10-15 19:32:54 +0530

    /

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const instance = axios.create({
   // baseURL: import.meta.env.VITE_API_BASE_URL,
    //baseURL: "http://localhost:5001/",
-  baseURL: 'https://invoice-analyser-6uzd.onrender.com',
+  baseURL: 'https://invoice-analyser-6uzd.onrender.com/',
   headers: {
     'Content-Type': 'application/json' 
   }
